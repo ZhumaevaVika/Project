5185376ef4b50f6404e0c94abc7d2e4a1ecdcb42
Zhumaeva Vika <s25781256@gmail.com>
2022-11-25 15:27:33 +0300

    No global keys

--- WorkHere.py
+++ WorkHere.py
@@ -3,9 +3,9 @@ from pygame.math import Vector2
 import math
 from random import randint, randrange, choice
 
-#TODO Сделать карту, чтобы экран по ней перемещался, а игрок был в центре
-#TODO Сделать игроку возможность стрелять
-#TODO Заспавнить на карте еду
+# TODO Сделать карту, чтобы экран по ней перемещался, а игрок был в центре
+# TODO Сделать игроку возможность стрелять
+# TODO Заспавнить на карте еду
 
 FPS = 60
 WIDTH = 1000
@@ -17,14 +17,17 @@ YELLOW = 0xFFC91F
 GREEN = 0x00FF00
 MAGENTA = 0xFF03B8
 CYAN = 0x00FFCC
-BLACK = 0x000000
+BLACK = (0, 0, 0)
 WHITE = 0xFFFFFF
 GREY = 0x7D7D7D
 GAME_COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
 class Player(pg.sprite.Sprite):
-    global keys
+    def if_move(self):
+        keys = pg.key.get_pressed()
+        if keys:
+            self.move(keys)
 
     def __init__(self):
         pos = (500, 375)
@@ -50,9 +53,9 @@ class Player(pg.sprite.Sprite):
             if event.pos[0] != self.pos.x:
                 self.angle = math.atan((event.pos[1] - self.pos.y) / (event.pos[0] - self.pos.x))
             elif event.pos[0] == self.pos.x:
-                self.angle = -80*(event.pos[1] - self.pos.y)/abs(event.pos[1] - self.pos.y)
-            self.angle = self.angle*360/6.28
-            if event.pos[0]<self.pos.x:
+                self.angle = -80 * (event.pos[1] - self.pos.y) / abs(event.pos[1] - self.pos.y)
+            self.angle = self.angle * 360 / 6.28
+            if event.pos[0] < self.pos.x:
                 self.angle += 180
             self.rotate()
 
@@ -63,10 +66,9 @@ class Player(pg.sprite.Sprite):
         # Rotate the offset vector.
         offset_rotated = self.offset.rotate(self.angle)
         # Create a new rect with the center of the sprite + the offset.
-        self.rect = self.image.get_rect(center=self.pos+offset_rotated)
+        self.rect = self.image.get_rect(center=self.pos + offset_rotated)
 
-
-    def move(self):
+    def move(self, keys):
         if keys[pg.K_w]:
             self.pos.y -= self.speed
         if keys[pg.K_a]:
@@ -76,6 +78,7 @@ class Player(pg.sprite.Sprite):
         if keys[pg.K_d]:
             self.pos.x += self.speed
         # FIXME При движении по диагонали скорость увеличивается в 2 раза, надо чтобы она оставалась постоянной
+
 
 class Food(pg.sprite.Sprite):
     def __init__(self, filename):
@@ -91,14 +94,13 @@ class Food(pg.sprite.Sprite):
         self.offset = Vector2(0, 0)  # We shift the sprite 50 px to the right.
         self.angle = 0
 
-        self.vx = randint(-10, 10)/100
-        self.vy = (0.0101-self.vx**2)**0.5*[-1,1][randrange(2)]
+        self.vx = randint(-10, 10) / 100
+        self.vy = (0.0101 - self.vx ** 2) ** 0.5 * [-1, 1][randrange(2)]
 
     def update(self, event):
         self.angle += 0.35
         self.rotate()
         self.move()
-
 
     def rotate(self):
         """Rotate the image of the sprite around a pivot point."""
@@ -107,30 +109,59 @@ class Food(pg.sprite.Sprite):
         # Rotate the offset vector.
         offset_rotated = self.offset.rotate(self.angle)
         # Create a new rect with the center of the sprite + the offset.
-        self.rect = self.image.get_rect(center=self.pos+offset_rotated)
+        self.rect = self.image.get_rect(center=self.pos + offset_rotated)
 
     def move(self):
         self.pos.x += self.vx
         self.pos.y += self.vy
+
 
 class Square(Food):
     def __init__(self):
         filename = 'Sprites/Square.png'
         super().__init__(filename)
 
+
 class Triangle(Food):
     def __init__(self):
         filename = 'Sprites/Triangle.png'
         super().__init__(filename)
 
+
 class Pentagon(Food):
     def __init__(self):
         filename = 'Sprites/Pentagon.png'
         super().__init__(filename)
+
     def update(self, event):
         self.angle += 0.15
         self.rotate()
         self.move()
+
+
+class Bullet(pg.sprite.Sprite):
+    def __init__(self, player):
+        super().__init__()
+        self.image = pg.Surface((122, 70), pg.SRCALPHA)
+        # A reference to the original image to preserve the quality.
+        self.orig_image = pg.image.load('Sprites/AlphaPentagon.png')
+        self.size = self.orig_image.get_size()
+        self.orig_image = pg.transform.scale(self.orig_image, (int(self.size[0] * 0.36), int(self.size[1] * 0.36)))
+        self.rect = self.orig_image.get_rect()
+        x, y = player.pos
+        self.x = x
+        self.y = y
+
+        self.vx = 1
+        self.vy = 1
+
+    def update(self, event):
+        self.move()
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+
 
 class Generator():
     def generate_player(self):
@@ -141,7 +172,7 @@ class Generator():
     def generate_food(self, all_sprites):
         N_max = 12
         N_prop = 1
-        variants = [0, 1, 0, 1, 0, 2, 0, 1, 0, 0, 0, 0]*N_prop
+        variants = [0, 1, 0, 1, 0, 2, 0, 1, 0, 0, 0, 0] * N_prop
         for i in range(N_max):
             food = choice(variants)
             if food == 0:
@@ -156,10 +187,7 @@ class Generator():
         return all_sprites
 
 
-
-
 def main():
-    global keys
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     clock = pg.time.Clock()
     generator = Generator()
@@ -174,15 +202,16 @@ def main():
                 return
             elif event.type == pg.MOUSEMOTION:
                 event_mouse = event
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                bullet = Bullet(player)
+                all_sprites.add(bullet)
+                print(all_sprites)
 
-        keys = pg.key.get_pressed()
-        if keys:
-            player.move()
+        player.if_move()
 
         all_sprites.update(event_mouse)
         screen.fill(WHITE)
         all_sprites.draw(screen)
-        pg.draw.circle(screen, (255, 128, 0), [int(i) for i in player.pos], 3)
         pg.display.flip()
         clock.tick(FPS)
 
