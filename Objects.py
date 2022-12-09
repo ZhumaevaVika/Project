f55f73a83d6e15600d00840fa82fd43271f1f491
physicistmaksim <113688831+physicistmaksim@users.noreply.github.com>
2022-12-10 01:30:57 +0300

    Upgrade bars added (without lags) Health bars for food added (with lags)

--- Objects.py
+++ Objects.py
@@ -636,8 +636,10 @@ class Food(pg.sprite.Sprite):
         self.vx = randint(-10, 10) / 100
         self.vy = (0.0101 - self.vx ** 2) ** 0.5 * [-1, 1][randrange(2)]
         self.HP = 10
+        self.max_HP = 10
         self.XP = 10
         self.m = 10
+        self.has_not_health_bar = True
 
     def update(self):
         self.move()
@@ -688,6 +690,7 @@ class Square(Food):
         filename = 'Sprites/square.png'
         super().__init__(filename)
         self.HP = 10
+        self.max_HP = 10
         self.BD = 8  # Body_damage 8 HP
         self.XP = 10  # Score
         self.r = 19
@@ -705,6 +708,7 @@ class Triangle(Food):
         filename = 'Sprites/triangle.png'
         super().__init__(filename)
         self.HP = 30
+        self.max_HP = 30
         self.BD = 8  # Body_damage 8 HP
         self.XP = 25  # Score
         self.r = 15
@@ -722,6 +726,7 @@ class Pentagon(Food):
         filename = 'Sprites/pentagon.png'
         super().__init__(filename)
         self.HP = 100
+        self.max_HP = 100
         self.BD = 12  # Body_damage
         self.XP = 130
         self.r = 27
@@ -739,6 +744,7 @@ class AlphaPentagon(Food):
         filename = 'Sprites/alpha_pentagon.png'
         super().__init__(filename)
         self.HP = 3000
+        self.max_HP = 3000
         self.BD = 20  # Body_damage
         self.XP = 3000
         self.r = 69
